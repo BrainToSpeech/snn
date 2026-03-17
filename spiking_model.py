615dcd2c793e51a651cd902979730078882b5424
Alex <awisniewska1206@gmail.com>
2026-03-17 09:35:57 +0900

    Update spiking_model.py

--- spiking_model.py
+++ spiking_model.py
@@ -1,3 +1,10 @@
 # This file contains the Spiking Model and all relevant modules
 # We use spikingjelly (Fang et al., 2023) for all SNN applications
 
+from spikingjelly.activation_based import surrogate
+
+def pick_surrogate(surrogate_name: str):
+    if surrogate_name == "atan":
+        return surrogate.ATan()
+    elif surrogate_name == "sigmoid":
+        return surrogate.Sigmoid()
