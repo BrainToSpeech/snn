d653c8b56f308423af8f3dcba4437033f89ccb9b
Alex <awisniewska1206@gmail.com>
2026-03-17 09:28:45 +0900

    Update neurons.py

--- models/neurons.py
+++ models/neurons.py
@@ -1,1 +1,9 @@
 # This file contains different versions of tested neuron models
+
+import numpy as np
+import torch
+import torch.nn as nn
+from spikingjelly.activation_based import neuron
+from spikingjelly.activation_based import surrogate
+import torch.nn.functional as F
+import random
