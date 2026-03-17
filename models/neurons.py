4dcd6d3ba99de20b47435f420b1bfcdf1af4be37
Alex <awisniewska1206@gmail.com>
2026-03-17 09:33:36 +0900

    Update neurons.py

--- models/neurons.py
+++ models/neurons.py
@@ -7,4 +7,24 @@ class LTLIFNode(neuron.ParametricLIFNode):
     '''
     LIF node
     '''
-    pass
+    def __init__(
+        self,
+        init_tau=2.0,
+        surrogate_function=None,
+        step_mode="m",
+        backend="torch",
+        v_threshold=1.0,
+        v_reset=0.0,
+        decay_input=False,
+    ):
+        
+        super().__init__(
+            init_tau=init_tau,
+            surrogate_function=surrogate_function,
+            step_mode=step_mode,
+            backend=backend,
+            v_threshold=v_threshold,
+            v_reset=v_reset,
+            decay_input=decay_input,
+        )
+        self.spike_rate = None
