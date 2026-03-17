b32f4ce9527efebd28cecb470d6bc16fb00637b5
Alex <awisniewska1206@gmail.com>
2026-03-17 09:31:11 +0900

    Update neurons.py

--- models/neurons.py
+++ models/neurons.py
@@ -1,12 +1,7 @@
 # This file contains different versions of tested neuron models
 
-import numpy as np
 import torch
 import torch.nn as nn
-from spikingjelly.activation_based import neuron
-from spikingjelly.activation_based import surrogate
-import torch.nn.functional as F
-import random
 
 class LTLIFNode(neuron.ParametricLIFNode):
     '''
